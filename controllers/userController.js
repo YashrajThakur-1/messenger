72b9aa1c3028e05a7b99269760802a35e9eea7e4
yashraj thakur <yashrajthakur009@gmail.com>
2024-06-15 13:02:54 +0530

    adding on git

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,6 +78,7 @@ module.exports.logOut = (req, res, next) => {
   try {
     if (!req.params.id) return res.json({ msg: "User id is required " });
     onlineUsers.delete(req.params.id);
+    res.status(200).json({ msg: "User Logout Succesfully" });
     return res.status(200).send();
   } catch (ex) {
     next(ex);
